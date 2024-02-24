19fb26ba9e2d167063b3d4afeb9649b848c4aa4e
baseinsc <157590659+baseinsc@users.noreply.github.com>
2024-02-24 22:10:38 +0800

    Update updateBlock.ts

--- src/updateBlock.ts
+++ src/updateBlock.ts
@@ -1,3 +1,4 @@
+import { Alchemy, Network } from "alchemy-sdk";
 import { handleResultToDb } from "./sync/txToDb"
 import { zero_address } from "./utils/address"
 import { fetchAccountTs, scanRowType } from "./utils/getTx"
@@ -5,65 +6,71 @@ import { dbGetBlockLast, dbSetBlockLast } from "./utils/io"
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const { ETH_SCAN_API_KEY_2, DEX_ADDRESS } = process.env;
+const { WEB3RPC_KEY, ETH_SCAN_API_KEY_2, DEX_ADDRESS } = process.env;
 
+const settings = {
+    apiKey: WEB3RPC_KEY,
+    network: Network.BASE_MAINNET,
+};
 export const updateBase20 = async () => {
     const getTxs = async () => {
-        await new Promise((ok) => setTimeout(ok, 5e3))
+        try {
+            await new Promise((ok) => setTimeout(ok, 1e3))
 
-        const startblock = await dbGetBlockLast()
-        console.log('startblock--->', startblock)
-        const result = await fetchAccountTs(zero_address, startblock)
-        const result_dex = await fetchAccountTs(DEX_ADDRESS, startblock, ETH_SCAN_API_KEY_2)
+            const alchemy = new Alchemy(settings);
+            const last_block = await alchemy.core.getBlockNumber()
+            const startblock = Number(await dbGetBlockLast())
+            const _endblock = startblock + 10000
+            const endblock = _endblock >= last_block ? last_block : _endblock 
+            const result = await fetchAccountTs(zero_address, startblock + '', void 0, endblock + '')
+            const result_dex = await fetchAccountTs(DEX_ADDRESS, startblock + '', ETH_SCAN_API_KEY_2, endblock + '')
 
-        if(result && result_dex){
-            if(typeof (result as any)?.result === 'object' && typeof (result_dex as any)?.result === 'object'){
-                const mergeResult = []
-                .concat((result as any)?.result || [])
-                .concat((result_dex as any)?.result || [])
-                .sort((x, y) => Number(x.timeStamp) - Number(y.timeStamp))
-
-                const _result = {
-                    status: '1',
-                    result: mergeResult as scanRowType[]
-                }
-                const lastBlock = await handleResultToDb(_result)
-
-                if(lastBlock){
-                    await dbSetBlockLast(lastBlock)
-                }
-                console.log('_result--->', _result.result.length)
-                if(!lastBlock || lastBlock == startblock){
-                    console.log('block null data ~~ ')
-                    await new Promise((ok) => setTimeout(ok, 5e3))
-                    const result = await fetchAccountTs(zero_address, (1 + Number(startblock)).toString(), void 0, "99999999", '2')
-                    const result_dex = await fetchAccountTs(DEX_ADDRESS, (1 + Number(startblock)).toString(), ETH_SCAN_API_KEY_2, "99999999", '2')
-                    const firstData1 = (result as any)?.result || []
-                    const firstData2 = (result_dex as any)?.result || []
-                    const minData: scanRowType[] = [...firstData1, ...firstData2]
+            if (result && result_dex) {
+                if (typeof (result as any)?.result === 'object' && typeof (result_dex as any)?.result === 'object') {
+                    const row1 = (result as any)?.result || []
+                    const row2 = (result_dex as any)?.result || []
+                    const _mergeResult: scanRowType[] = []
+                        .concat(row1)
+                        .concat(row2)
+                        .sort((x, y) => Number(x.timeStamp) - Number(y.timeStamp))
                     
-                    if(minData.length){
-                        const minBlockNumber = Math.min(...minData.map(e => Number(e.blockNumber)))
-                        
-                        console.log('---minBlockNumber---', minBlockNumber)
-                        await dbSetBlockLast(minBlockNumber.toString())
-                    }else{
-                        console.log('last not data')
+                    let minBlockNumber = 99999999999
+                    if(row1.length && row2.length){
+                        const minBlockNumber1 = Math.max(...row1.map(e => Number(e.blockNumber)))
+                        const minBlockNumber2 = Math.max(...row2.map(e => Number(e.blockNumber)))
+                        minBlockNumber = Math.min(minBlockNumber1, minBlockNumber2)
                     }
+
+                    const mergeResult = _mergeResult.filter((e) => Number(e.blockNumber) <= minBlockNumber)
+                    const _result = {
+                        status: '1',
+                        result: mergeResult
+                    }
+                    const lastBlock = await handleResultToDb(_result)
+
+                    if (lastBlock) {
+                        await dbSetBlockLast((Number(lastBlock) + 1).toString())
+                    }
+                    if(!lastBlock && mergeResult.length === 0){
+                        await dbSetBlockLast(endblock + 1 + '')
+                    }
+                    getTxs()
+                } else {
+                    console.log('(result as any)?.status', (result as any)?.status)
+                    await new Promise((ok) => setTimeout(ok, 5e3))
+                    getTxs()
                 }
-                await new Promise((ok) => setTimeout(ok, 5e3))
-                getTxs()
-            }else{
-                console.log('(result as any)?.status', (result as any)?.status)
+            } else {
+                console.log('not new data')
                 await new Promise((ok) => setTimeout(ok, 5e3))
                 getTxs()
             }
-        }else{
-            console.log('not new data')
-            await new Promise((ok) => setTimeout(ok, 5e3))
+
+        } catch (error) {
+            console.log("error::", error)
             getTxs()
         }
     }
     getTxs()
-    
+
 }
